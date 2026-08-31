b522effe1a90db4e1b22019421e748d45259183f
Svess <Svess@Svesse1312.local>
2026-08-31 23:32:08 +0200

    Enable web and Expo tunnel preview

---
+++ metro.config.js
@@ -0,0 +1,14 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts.push('wasm');
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    return middleware(req, res, next);
+  };
+};
+
+module.exports = config;
